feab8916430c3ff3c793363ce5082e88b05d71c7
OctopusLian <zoctopus@qq.com>
2025-03-18 22:57:22 +0800

    :art: 调度任务的并发执行

--- batch/src/main/java/com/jiava/train/batch/job/TestJob.java
+++ batch/src/main/java/com/jiava/train/batch/job/TestJob.java
@@ -1,9 +1,12 @@
 package com.jiava.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+// @DisallowConcurrentExecution 禁止并发执行
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
